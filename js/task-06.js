ffb1b0cbe3c6ff6d2d4347f4d8dcce4220384f14
Liubov12 <68028382+Liubov12@users.noreply.github.com>
2021-08-15 11:36:09 +0200

    изменить 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,23 +1,15 @@
-const inputRef = document.querySelector('input#validation - input');
+const inputRef = document.querySelector('input#validation-input');
 const stringLength = inputRef.getAttribute('data-length');
 
-function inputValidation(event) {
-    
-    if(inputRef.value.length == stringLength) {
+function inputValidation(event) { 
+    let inputString = inputRef.getAttribute('value');
+    if (inputRef.value.length == stringLength) {
         inputRef.classList.add('valid');
         inputRef.classList.remove('invalid');
-    }
-    else{
+    } else {
         inputRef.classList.add('invalid');
-        inputRef.classList.remove('valid')
+        inputRef.classList.remove('valid');
     }
-    if(/^\s + $/.test(event.target.value)) {
-        inputRef.value = '';
-    }
+};
 
-{inputRef.classList.remove('invalid');}
-    if (inputRef.value.length === 0) {
-        inputRef.classList.remove('invalid');    
-    }
-}
 inputRef.addEventListener('change', inputValidation);
